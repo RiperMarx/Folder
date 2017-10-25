59fe2cacbdb52d5a92d61b3c676b523b27891aed
Jorge Martínez Carranza <jhmc000@hotmail.com>
2017-10-25 14:05:19 -0500

    Programa para mostrar valores de los nombres de archivos de texto en un folder

--- Folder/Form1.Designer.cs
+++ Folder/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Dgv_Grid = new System.Windows.Forms.DataGridView();
+            this.Btn_Buscar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Dgv_Grid);
+            this.panel1.Controls.Add(this.Btn_Buscar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(453, 326);
+            this.panel1.TabIndex = 0;
+            // 
+            // Dgv_Grid
+            // 
+            this.Dgv_Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Grid.Location = new System.Drawing.Point(20, 105);
+            this.Dgv_Grid.Name = "Dgv_Grid";
+            this.Dgv_Grid.Size = new System.Drawing.Size(421, 209);
+            this.Dgv_Grid.TabIndex = 1;
+            // 
+            // Btn_Buscar
+            // 
+            this.Btn_Buscar.Location = new System.Drawing.Point(170, 55);
+            this.Btn_Buscar.Name = "Btn_Buscar";
+            this.Btn_Buscar.Size = new System.Drawing.Size(102, 44);
+            this.Btn_Buscar.TabIndex = 0;
+            this.Btn_Buscar.Text = "Buscar Nombres de Archivos";
+            this.Btn_Buscar.UseVisualStyleBackColor = true;
+            this.Btn_Buscar.Click += new System.EventHandler(this.Btn_Buscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(453, 326);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Folders";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Grid)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView Dgv_Grid;
+        private System.Windows.Forms.Button Btn_Buscar;
     }
 }
 
--- Folder/Form1.cs
+++ Folder/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Folder
 {
@@ -15,6 +16,51 @@ namespace Folder
         public Form1()
         {
             InitializeComponent();
+        }
+        public class VGlobales {
+            //Variable tipo
+            public static string tipo  { get; set; }
+        }
+
+        //Metodo para buscar nombres de folders
+        public void Buscar()
+        {
+            //Variable folder
+            string folder = "";
+            //Inicializacion de FolderBrowserDialog
+            FolderBrowserDialog buscarf = new FolderBrowserDialog();
+            //Desactivacion de NewFolderButton
+            buscarf.ShowNewFolderButton = false;
+            //Validacion de cuadro de dialogo
+            if (buscarf.ShowDialog() == DialogResult.OK)
+            {
+                folder = buscarf.SelectedPath;
+                //Inicializacion de DirectoryInfo dando el camino variable "folder"
+                var di = new DirectoryInfo(folder);
+                //Obtiene los archivos con la variable di y los asigna a la variable files
+                var files = di.GetFiles("*.txt");
+                //Inicializacion de un DataTable llamado Dtable
+                DataTable Dtable = new DataTable();
+                //Asignacion de columna Archivos en la DataTable y su tipo a texto
+                Dtable.Columns.Add("Archivos",typeof (string));
+                //Se inicializa una fila llamada line
+                DataRow line;
+                //Loop para listar los archivos en el folder
+                foreach (var obj in files)
+                {
+                    line = Dtable.NewRow();
+                    line["Archivos"] = obj.ToString();
+                    Dtable.Rows.Add(line);
+                }
+                MessageBox.Show("Listo");
+                //Asigna la Data?Table para mostrarse en un DataGridView
+                Dgv_Grid.DataSource = Dtable;
+            }
+        }
+
+        private void Btn_Buscar_Click(object sender, EventArgs e)
+        {
+            Buscar();
         }
     }
 }
